0264cf891af63141e6b7abf6e82b1ddc1c33a646
andre <andre@DESKTOP-UMP1I67>
2019-01-27 16:29:16 -0200

    Teste

---
+++ befast-projeto/src/main/java/com/befast/springboot/befastprojeto/BefastProjetoApplication.java
@@ -0,0 +1,14 @@
+package com.befast.springboot.befastprojeto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BefastProjetoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BefastProjetoApplication.class, args);
+	}
+
+}
+
---
+++ befast-projeto/src/main/java/com/befast/springboot/befastprojeto/jwt/resource/AuthenticationException.java
@@ -0,0 +1,7 @@
+package com.befast.springboot.befastprojeto.jwt.resource;
+
+public class AuthenticationException extends RuntimeException {
+	public AuthenticationException(String message, Throwable cause) {
+		super(message, cause);
+	}
+}
