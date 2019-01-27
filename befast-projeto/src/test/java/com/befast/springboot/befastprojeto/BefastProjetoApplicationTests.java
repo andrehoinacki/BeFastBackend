cde41fa2819c8e69f3fb309348ca2326e34a78db
andre <andre@DESKTOP-UMP1I67>
2019-01-27 16:29:16 -0200

    Teste

---
+++ befast-projeto/src/test/java/com/befast/springboot/befastprojeto/BefastProjetoApplicationTests.java
@@ -0,0 +1,17 @@
+package com.befast.springboot.befastprojeto;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BefastProjetoApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
