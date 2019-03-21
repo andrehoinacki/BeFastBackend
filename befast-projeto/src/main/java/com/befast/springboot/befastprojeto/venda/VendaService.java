8ec908b3505b0243aae122c6ad11028fd5d1e369
Jessica <jessica.jdon@gmail.com>
2019-03-20 21:19:41 -0300

    Atualizando quantidade de produro e credito do usuário após a venda

--- befast-projeto/src/main/java/com/befast/springboot/befastprojeto/venda/VendaService.java
+++ befast-projeto/src/main/java/com/befast/springboot/befastprojeto/venda/VendaService.java
@@ -4,6 +4,12 @@ import java.util.List;
 
 import javax.transaction.Transactional;
 
+import com.befast.springboot.befastprojeto.admin.produto.ProdutoRepository;
+import com.befast.springboot.befastprojeto.admin.saldo.Saldo;
+import com.befast.springboot.befastprojeto.admin.usuario.Usuario;
+import com.befast.springboot.befastprojeto.admin.usuario.UsuarioRepository;
+import com.befast.springboot.befastprojeto.venda.pagamento.FormaPagamento;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
@@ -13,6 +19,10 @@ public class VendaService {
 	
 	@Autowired
 	private VendaRepository vendaRepository;
+	@Autowired
+	private UsuarioRepository usuarioRepository;
+	@Autowired
+	private ProdutoRepository produtoRepository;
 
 	@Cacheable("list-venda")
 	public List<Venda> list(){
@@ -27,8 +37,16 @@ public class VendaService {
 		if (venda.getCliente() == null || venda.getFuncionario() == null ) {
 			throw new Exception("ERROR_CAMPOS_OBRIGATORIOS");
 		}
-				
-		return vendaRepository.save(venda);
+		Venda vendaRetorno = vendaRepository.save(venda);
+		//ATUALIZA SALDO USUÁRIO
+		usuarioRepository.save(venda.getCliente());
+
+		//ATUALIZA PRODUTO
+		for (ItemVenda vi : venda.getItens()) {
+			produtoRepository.save(vi.getProduto());
+		}
+
+		return vendaRetorno;
 	}
 
 }
